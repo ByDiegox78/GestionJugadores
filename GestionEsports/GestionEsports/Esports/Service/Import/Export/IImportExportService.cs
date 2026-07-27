2a897fafb42fdc9bca867585cece170c20849272
ByDiegox78 <oms04081974@gmail.com>
2026-07-27 13:21:11 +0200

    FEAT: Implementacion del servicio de Import/Export

---
+++ GestionEsports/GestionEsports/Esports/Service/Import/Export/IImportExportService.cs
@@ -0,0 +1,12 @@
+﻿using CSharpFunctionalExtensions;
+using GestionEsports.Esports.Error.Common;
+using GestionEsports.Esports.Models;
+
+namespace GestionEsports.Esports.Service.Import.Export;
+
+public interface IImportExportService {
+    Result<int, DomainError> ExportarDatos(IEnumerable<Jugador> jugadors, string path);
+    Result<IEnumerable<Jugador>, DomainError> ImportarDatos(string path);
+    Result<int, DomainError> ExportarDatosSistema(IEnumerable<Jugador> jugadores);
+    Result<IEnumerable<Jugador>, DomainError> ImportarDatosSistema(string path);
+}
---
+++ GestionEsports/GestionEsports/Esports/Service/Import/Export/ImportExportService.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using GestionEsports.Esports.Error.Common;
+using GestionEsports.Esports.Models;
+using GestionEsports.Esports.Storage.Common;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace GestionEsports.Esports.Service.Import.Export;
+
+public class ImportExportService(IStorage<Jugador> storage) : IImportExportService{
+    private readonly ILogger _logger = Log.ForContext<ImportExportService>();
+    public Result<int, DomainError> ExportarDatos(IEnumerable<Jugador> jugadors, string path) {
+        _logger.Information("Exportando los datos del sistema a un archivo {Path}", path);
+        var list = jugadors.ToList();
+        return storage.Salvar(list, path)
+            .Map(r => list.Count);
+    }
+    public Result<IEnumerable<Jugador>, DomainError> ImportarDatos(string path) {
+        _logger.Information("Importando los datos del archivo {Path}", path);
+        return storage.Cargar(path);
+    }
+    public Result<int, DomainError> ExportarDatosSistema(IEnumerable<Jugador> jugadores) {
+        _logger.Information("Exportando los datos del sistema a un archivo");
+        return ExportarDatos(jugadores, string.Empty);
+    }
+    public Result<IEnumerable<Jugador>, DomainError> ImportarDatosSistema(string path) {
+        _logger.Information("Importando los datos del archivo {Path}", path);
+        return ImportarDatos(path);
+    }
+}
